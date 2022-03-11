569b80c8d3969a38360f2ec61a90517f468565e1
Magda098 <magjac22@gmail.com>
2022-03-11 12:37:10 -0800

    combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,9 +6,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-
 {
-
 int p = 48, q = 48, y = 48, x = 49;
 while (p < 58)
 {
@@ -17,25 +15,32 @@ while (q < 58)
 while (y < 58)
 {
 while (x < 58)
-{																putchar(p);															putchar(q);
-putchar(' ');															putchar(y);
-putchar(x);															if (!(p == 57 &&
-q== 56 &&
+{
+putchar(p);
+putchar(q);
+putchar(' ');
+putchar(y);
+putchar(x);
+if (!(p == 57 &&
+q == 56 &&
 y == 57 &&
 x == 57))
-{
 putchar(',');
 putchar(' ');
-}x++;																}
+}
+x++;
+}
 y++;
-x = 48;																}
+x = 48;
+}
 q++;
-q = p;																x = q + 1;
+q = p;
+x = q + 1;
 }
 p++;
-q= 48;				
+q = 48;
 y = p;
-x= q+ 1;}
+x = q+ 1;
 }
 putchar('\n');
 return (0);
