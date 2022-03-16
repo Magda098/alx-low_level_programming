e7100a365cb3883586f37223017777867444503f
Magda098 <magjac22@gmail.com>
2022-03-16 08:39:00 -0700

    prints the first 98 Fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,12 +10,10 @@ int count;
 unsigned long fib1 = 0, fib2 = 1, sum;
 unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
 unsigned long half1, half2;
-
 for (count = 0; count < 92; count++)
 {
 sum = fib1 + fib2;
 printf("%lu, ", sum);
-
 fib1 = fib2;
 fib2 = sum;
 }
@@ -29,7 +27,8 @@ half1 = fib1_half1 + fib2_half1;
 half2 = fib1_half2 + fib2_half2;
 if (fib1_half2 + fib2_half2 > 9999999999)
 {
-half1 += 1;															half2 %= 10000000000;
+	half1+=1;
+	half2 %= 10000000000;
 }
 printf("%lu%lu", half1, half2);
 if (count != 98)
